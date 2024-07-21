a89cb4e9c3a6b95530eda8b9596024be4aa1188f
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-07-21 23:23:49 +0530

    Update minimum-number-of-moves-to-seat-everyone.java

--- LeetCode/Sorting/Easy/minimum-number-of-moves-to-seat-everyone.java
+++ LeetCode/Sorting/Easy/minimum-number-of-moves-to-seat-everyone.java
@@ -16,3 +16,23 @@ class Solution {
     }
 
 }
+
+
+
+//Method-2
+
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int diff = 0;
+        int sum =0;
+        int n = seats.length;
+
+        for(int i=0;i<=n-1;i++)
+           sum += Math.abs(seats[i]- students[i]);
+
+      return sum;
+    }
+
+}
